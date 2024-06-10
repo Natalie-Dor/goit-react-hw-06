84659f00f3c6618826e13b3392bbd83e51ca78ff
Natalie-Dor <varnata000@gmail.com>
2024-06-10 16:09:17 +0300

    333

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,15 +3,12 @@ import { selectNameFilter } from '../../redux/filtersSlice';
 import { selectContacts } from '../../redux/contactsSlice';
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
-// import contacts from '../../contacts.json';
 selectContacts;
 export default function ContactList() {
   const contacts = useSelector(selectContacts);
   const nameFilter = useSelector(selectNameFilter);
 console.log(contacts)
-  // const savedContacts = contacts.filter(item => {
-  //   item.name.toLowerCase().includes(nameFilter.toLowerCase());
-  // });
+ 
   const savedContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(nameFilter.toLowerCase())
   );
